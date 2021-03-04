03a127d8b33aa4ff8e5883afd8e7a9ab1df2c985
Ilya Usov <iusov41@gmail.com>
2021-03-04 08:29:57 +0300

    reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n<0){
+        n=Math.abs(n);
+    }
+    let str = n.toString();
+    let arr = [];
+    arr = str.split('');
+    arr.reverse();
+    str= arr.join('');
+    return +str;
 }
